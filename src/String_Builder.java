e585f9ef0dc71f8c10192c0a51b8a6d5d1a7dc76
Emmanouil Richman <erichman966@gmail.com>
2020-11-12 23:54:50 -0500

    Added Constructors

--- src/String_Builder.java
+++ src/String_Builder.java
@@ -1,6 +1,26 @@
 import java.util.ArrayList;
 
 public class String_Builder {
+    private final int maxSize;
+    private ArrayList<String> builder;
 
+    public String_Builder() {
+        maxSize = Integer.MAX_VALUE;
+        builder = new ArrayList();
+    }
+
+    public String_Builder(int capacity) {
+        maxSize = capacity;
+        builder = new ArrayList<>(capacity);
+    }
+
+    public String_Builder(String str) {
+        maxSize = Integer.MAX_VALUE;
+        builder.add(str);
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
 
 }
